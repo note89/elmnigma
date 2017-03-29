c51dd9c9571fa71678cf8de64eabdb0307e1970e
Nils Eriksson <nilseriksson89.ne@gmail.com>
2017-03-29 20:38:19 +0200

    encode a to b

--- Main.elm
+++ Main.elm
@@ -2,6 +2,7 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Events exposing (onInput)
+import String exposing (toUpper, contains)
 
 
 main : Program Never Model Msg
@@ -20,12 +21,26 @@ main =
 
 type alias Model =
     { str : String
+    , plugboard : Pair
     }
+
+
+type EnigmaLetter
+    = A
+    | B
+
+
+type alias Pair =
+    ( EnigmaLetter, EnigmaLetter )
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model "", Cmd.none )
+    ( { str = ""
+      , plugboard = ( A, B )
+      }
+    , Cmd.none
+    )
 
 
 
@@ -36,11 +51,75 @@ type Msg
     = Set String
 
 
+fromEnigmaLetter : EnigmaLetter -> Char
+fromEnigmaLetter u =
+    case u of
+        A ->
+            'A'
+
+        B ->
+            'B'
+
+
+fromEnigmaLetters : List EnigmaLetter -> String
+fromEnigmaLetters engimaLetterList =
+    engimaLetterList
+        |> List.map (\c -> fromEnigmaLetter c)
+        |> String.fromList
+
+
+toEnigmaLetter : Char -> Maybe EnigmaLetter
+toEnigmaLetter c =
+    case c of
+        'A' ->
+            Just A
+
+        'B' ->
+            Just B
+
+        _ ->
+            Nothing
+
+
+toEnigmaLetters : String -> List EnigmaLetter
+toEnigmaLetters str =
+    str
+        |> String.toList
+        |> List.filterMap toEnigmaLetter
+
+
+matchAndFlip : Model -> EnigmaLetter -> EnigmaLetter
+matchAndFlip model c =
+    if Tuple.first model.plugboard == c then
+        Tuple.second model.plugboard
+    else
+        Tuple.first model.plugboard
+
+
+
+-- encode : String -> String
+
+
+encode matchFnc str =
+    str
+        |> toUpper
+        |> toEnigmaLetters
+        |> List.map matchFnc
+        |> fromEnigmaLetters
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Set str ->
-            ( Model str, Cmd.none )
+            let
+                matchFnc =
+                    matchAndFlip model
+
+                newStr =
+                    encode matchFnc str
+            in
+                ( { model | str = newStr }, Cmd.none )
 
 
 
@@ -59,7 +138,8 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
     div []
-        [ text model.str
-        , input [ onInput Set ]
+        [ input [ onInput Set ]
             []
+        , br [] []
+        , text model.str
         ]
